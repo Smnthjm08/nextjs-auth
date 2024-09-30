7e569b82c5590877b3ba35e49bb31cd17eb779fd
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-09-30 23:42:54 +0530

    feat: role-based access control added

--- auth.ts
+++ auth.ts
@@ -1,8 +1,18 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 
 import { db } from "./lib/db";
+import { getUserByid } from "./data/user";
+
+declare module "@auth/core" {
+  interface Session {
+    user: {
+      role: string;
+    } & DefaultSession["user"];
+  }
+}
 
 export const {
   auth,
@@ -12,12 +22,27 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
+      console.log({ sessiontoken: token });
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        //refer the next-auth.d.ts for reference for extended types
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserByid(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
